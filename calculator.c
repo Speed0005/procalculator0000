d2676272c44fb490ff413aa8c8318b61be0a6e2b
Speed <78754919+Speed0005@users.noreply.github.com>
2022-12-10 21:42:28 +0200

    added comments
    added comments bc to some people the code would be too shit to understand (its my guess) (anyway who the  heck would want to do anything with this)

--- calculator.c
+++ calculator.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 int main(){
+    //assigning values
     char operator;
     double num1;
     double num2;
     double result;
     int something;
 
+    //receiving user input
     printf("\nWhat type of operation do you want to do? (+ - * /)\n");
     scanf("%c", &operator);
 
@@ -15,7 +17,8 @@ int main(){
     scanf("%lf", &num1);
     printf("Enter number 2: ");
     scanf("%lf", &num2);
-
+    
+    //calculating the result (also switch for detecting operators), and printing it out
     switch (operator)
     {
     case '+':
@@ -39,6 +42,7 @@ int main(){
         break;
     }
     
+    //getting user input so the program doesnt automatically print things out and then close
     printf("\nInput something and press enter to exit...");
     scanf("%i", &something);
 
